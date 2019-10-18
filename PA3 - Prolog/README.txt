7b02a99e6ce3f38178b0ad641903696b362befe4
Hunter Cleary <34077789+hncleary@users.noreply.github.com>
2019-10-17 23:54:09 -0500

    Finished ShortestPath Prolog Assignment

---
+++ PA3 - Prolog/README.txt
@@ -0,0 +1,19 @@
+Hunter Cleary
+625001547
+
+How to Run:
+Predicate "path(A,B,P)." will return the shortest path P between two
+nodes on a graph given the starting node A and ending node B.
+
+Resources:
+http://www.learnprolognow.org/lpnpage.php?pageid=online
+https://www.swi-prolog.org/pldoc/man?section=opsummary
+https://stackoverflow.com/questions/3965054/prolog-find-minimum-in-a-list
+https://rlgomes.github.io/work/prolog/2012/05/22/19.00-prolog-and-graphs.html
+https://www.cpp.edu/~jrfisher/www/prolog_tutorial/2_15.html
+https://stackoverflow.com/questions/21161624/define-graph-in-prolog-edge-and-path-finding-if-there-is-a-path-between-two-ve
+
+
+A statement of the Aggie Code of Honor:
+An Aggie does not lie, cheat or steal or tolerate those who do.
+
--- PA3 - Prolog/shortestPath.pl
+++ PA3 - Prolog/shortestPath.pl
@@ -13,29 +13,58 @@
 % Example Graph #1
 arc(m,p,8).
 arc(q,p,13).
-arc(q,m,5).
+arc(q,m,6).
 arc(k,q,3).
-
-
-
-
+% graph 2
+arc(x,y,1000).
+arc(x,z,2).
+arc(x,w,2).
+arc(w,y,1).
+arc(z,y,2).
 % there is a connnection between X and Y if there is a weighted edge between the two
 connection(X,Y,W,[X,Y], _) :- arc(X,Y,W).
 % member(term, list) -- succeeds if the term unifies with a member in the list
 %  "\+" -- true if the goal cannot be proven
+% member true if X exists in the list V
 connection(X,Y,W,[X|P], V) :- \+ member(X,V),
   arc(X,Z,W1),
   connection(Z,Y,W2, P, [X|V]),
   W is W1 + W2.
-
-% path(A,B,[A,B], _) :- arc(A,B,W).
-% path(A,B,[X|P], V) :- \+ member(X,V),
-%   arc(A,C,W1),
-%   path(C,B,W2, P, [X|V]),
-%   W is W1 + W2.
-
-path(A,B,P) :- connection(A,B,W,P,[]).
-minPathWeight(A,B,P,W) :- connection(A,B,W,P,[]).
-
+% path(A,B,P) :- connection(A,B,_,P,[]).
+% Get the Minimum of a list
+% minl(List, Minimum).
+minValue([SingleValueList], SingleValueList).
+minValue([Start|End], Minimum) :-
+  minValue(End, EndMinimum),
+  Minimum is min(Start, EndMinimum).
+% get the shortest path out of
+% the possible path list
+shortestPathValue(A,B,Path,Weight) :-
+  %  list of all instances saitisyfin the goal for the template
+  setof([P,L],pathShortest(A,B,P,L),List),
+  List = [_|_],
+  smallestWeight(List,[Path,Weight]).
+% finding the minimum from set
+% given path and Weight
+smallestWeight([Left|Right],Min) :- minimum(Right,Left,Min).
+% minimal path
+% if there is only one value, it is the minimum
+minimum([],EqualValue,EqualValue).
+minimum([[Path,PossibleMin]|NextValue],[_,Min],Minimum) :-
+  % if the value is lower than the current minimum
+  PossibleMin < Min,
+  % check the remaining values to see if there
+  % lower than the current minimum
+  currentMin(NextValue,[Path,PossibleMin],Minimum).
+  % EndMinimum is min(Minimum, Minimum).
+% min is "current minimum" Minimum is returned minimum
+minimum([_|NextValue],Min,Minimum) :-
+    minimum(NextValue,Min,Minimum).
+% current min recursively runs the minimum to check
+% all possible outcomes
+currentMin(V1,[P,V2],M) :- minimum(V1,[P,V2],M).
+% predicate to connect smallest weight to connection
+pathShortest(A,B,P,L) :- connection(A,B,L,P,[]).
 % connection(k,p,Weight,Path, []).
 % path(k,p,Path, []).
+path(A,B,P) :- shortestPathValue(A,B,P,_).
